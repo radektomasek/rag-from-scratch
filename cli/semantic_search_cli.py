15ed3cd8834ee852f1bef2c00fccdd630a009cb9
Radek Tomasek <radek.tomasek@gmail.com>
2025-11-26 16:42:36 -0800

    Added basic implementation of the semantic search

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,10 +1,22 @@
 import argparse
 
+from lib.semantic_search import (
+    SemanticSearch,
+    verify_model
+)
+
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    subparsers.add_parser("verify", help="Print the model information for the vector search")
     args = parser.parse_args()
 
+    semantic_search = SemanticSearch()
+
     match args.command:
+        case "verify":
+            verify_model(semantic_search)
         case _:
             parser.print_help()
 
